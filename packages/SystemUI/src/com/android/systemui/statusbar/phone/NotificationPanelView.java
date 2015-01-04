62ed32f3b53893a179427bd4048c2999b7e65a51
David96 <davidlepplaweber@aospa.co>
2015-01-05 00:27:56 +0100

    QS: fix tiles not being refreshed on overscrolling
    This fixes https://code.google.com/p/android-developer-preview/issues/detail?id=1860 I will later submit that to AOSP as well.
    
    PS2: better approach without exposing private method
    
    Change-Id: I53fa9cd9244d690f317da61bd4de89aa07915a56
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -732,10 +732,6 @@ public class NotificationPanelView extends PanelView implements
                 && event.getY(event.getActionIndex()) < mStatusBarMinHeight) {
             mQsExpandImmediate = true;
             requestPanelHeightUpdate();
-
-            // Normally, we start listening when the panel is expanded, but here we need to start
-            // earlier so the state is already up to date when dragging down.
-            setListening(true);
         }
         super.onTouchEvent(event);
         return true;
@@ -1632,8 +1628,6 @@ public class NotificationPanelView extends PanelView implements
         mScrollYOverride = -1;
         if (mExpandedHeight == 0f) {
             setListening(false);
-        } else {
-            setListening(true);
         }
         mQsExpandImmediate = false;
         mTwoFingerQsExpandPossible = false;
@@ -1678,6 +1672,7 @@ public class NotificationPanelView extends PanelView implements
                 || mStatusBar.getBarState() == StatusBarState.SHADE_LOCKED) {
             mAfforanceHelper.animateHideLeftRightIcon();
         }
+        setListening(true);
     }
 
     @Override
