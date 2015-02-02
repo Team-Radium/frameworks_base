e4b589ee8ed779536862d9e50f05d802373c9ce3
lichti1901 <lichti190185@gmail.com>
2015-02-02 19:28:48 +0100

    Navigation bar button color (1/2)
    Change-Id: I5dfd9f09c78fb685e4fceb166f52fd0d3cfe482b
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2708,6 +2708,12 @@ public final class Settings {
         @Deprecated
         public static final String DEV_FORCE_SHOW_NAVBAR = Secure.DEV_FORCE_SHOW_NAVBAR;
 
+	/**
+         * Navigation bar button color
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_TINT = "navigation_bar_tint";
+
         /**
          * Date format string
          *   mm/dd/yyyy
