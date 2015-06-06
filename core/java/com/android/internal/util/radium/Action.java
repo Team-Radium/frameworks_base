CyanideL/android_frameworks_base

fb: TRDS 5.0 (1/4)
patchset: add holodark resources

patchset: add to slim actions

patchset: import Toast library in Action.java

patchset: some tweaks and fixes

patchset: change auto detect light conditions method

patchset: fix possible UI freeze on auto mode change

patchset: import ServiceManager library into uimodemanagerservice

patchset: fix aapt shit

patchset: fix context in UiModeManagerService?

patchset: set mContext to BroadcastReceiver's context,
        add toasts to debug light sensor stuff

patchset: adjust DARK_CONDITION value

patchset: clean up toasts

patchset: fix NPE in Resources.java?

PS20: update BridgePowerManager to fix SDK build

PS21: revert to original BridgePowerManager (if building SDK tools,
        need to use patchset 20 or it will error out)

PS22: more debugging toasts grrrrr

PS23: change holodark to darktheme, hololight to lighttheme.
        remove holodark resources.

PS24: add darktheme resources.

PS26: qs tile

PS27: reset theme mode on longpress qs tile

PS: fix qs icon

CyanideL: Fix up to work ALONGSIDE theme engine
Change-Id: Ic88838247c47d2a92b55d1cb1b00330f2d62fb88

Conflicts:
	core/java/android/app/ResourcesManager.java
	core/java/android/content/res/Resources.java
	core/java/com/android/internal/util/slim/Action.java
	core/java/com/android/internal/util/slim/ActionHelper.java
	core/res/res/values/slim_strings.xml
	core/res/res/values/slim_symbols.xml
	packages/SystemUI/res/values/config.xml
	packages/SystemUI/res/values/slim_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

core/java/com/android/internal/util/cyanide/Action.java
@@ -19,8 +19,10 @@ package com.android.internal.util.cyanide;
 import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.SearchManager;
+import android.app.IUiModeManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.Intent;
 import android.hardware.input.InputManager;
 import android.hardware.TorchManager;
@@ -42,6 +44,7 @@ import android.view.IWindowManager;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
+import android.widget.Toast;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -75,6 +78,15 @@ public class Action {
                     ServiceManager.getService(Context.STATUS_BAR_SERVICE));
             if (barService == null) {
                 return; // ouch
+            }
+
+            if (collapseShade) {
+                if (!action.equals(ActionConstants.ACTION_THEME_SWITCH)) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
+                    }
+                }
             }
 
             final IWindowManager windowManagerService = IWindowManager.Stub.asInterface(
@@ -264,6 +276,36 @@ public class Action {
                         (PowerManager) context.getSystemService(Context.POWER_SERVICE);
                 if (!powerManager.isScreenOn()) {
                     powerManager.wakeUp(SystemClock.uptimeMillis());
+                }
+                return;
+            } else if (action.equals(ActionConstants.ACTION_THEME_SWITCH)) {
+                boolean autoLightMode = Settings.Secure.getIntForUser(
+                        context.getContentResolver(),
+                        Settings.Secure.UI_THEME_AUTO_MODE, 0,
+                        UserHandle.USER_CURRENT) == 1;
+                boolean state = context.getResources().getConfiguration().uiThemeMode
+                        == Configuration.UI_THEME_MODE_HOLO_DARK;
+                if (autoLightMode) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
+                    }
+                    Toast.makeText(context,
+                            com.android.internal.R.string.theme_auto_switch_mode_error,
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Handle a switch change
+                // we currently switch between darktheme and lighttheme till either
+                // theme engine is ready or lighttheme is ready. Currently due of
+                // missing light themeing lighttheme = system base theme
+                final IUiModeManager uiModeManagerService = IUiModeManager.Stub.asInterface(
+                        ServiceManager.getService(Context.UI_MODE_SERVICE));
+                try {
+                    uiModeManagerService.setUiThemeMode(state
+                            ? Configuration.UI_THEME_MODE_HOLO_LIGHT
+                            : Configuration.UI_THEME_MODE_HOLO_DARK);
+                } catch (RemoteException e) {
                 }
                 return;
             } else if (action.equals(ActionConstants.ACTION_TORCH)) {

core/java/com/android/internal/util/cyanide/ActionHelper.java
@@ -304,6 +304,9 @@ public class ActionHelper {
         } else if (clickAction.equals(ActionConstants.ACTION_SCREENSHOT)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_screenshot", null, null);
+        } else if (clickAction.equals(ActionConstants.ACTION_THEME_SWITCH)) {
+            resId = systemUiResources.getIdentifier(
+                        SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_theme_switch", null, null);
         } else {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_null", null, null);

Team-Radium/frameworks_base

Signed-off-by: OwnDroid <askowndroid@gmail.com>
	core/java/com/android/internal/util/temasek/Action.java
	core/java/com/android/internal/util/temasek/ActionHelper.java
	core/res/res/values/cr_strings.xml
	core/res/res/values/cr_symbols.xml
	packages/SystemUI/res/values/cr_strings.xml
	core/res/res/values/cm_symbols.xml

core/java/com/android/internal/util/radium/Action.java
@@ -19,8 +19,10 @@ package com.android.internal.util.radium;
 import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.SearchManager;
+import android.app.IUiModeManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.Intent;
 import android.hardware.input.InputManager;
 import android.media.AudioManager;
@@ -40,6 +42,7 @@ import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
+import android.widget.Toast;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -73,6 +76,15 @@ public class Action {
                     ServiceManager.getService(Context.STATUS_BAR_SERVICE));
             if (barService == null) {
                 return; // ouch
+            }
+
+            if (collapseShade) {
+                if (!action.equals(OwnActionConstants.ACTION_THEME_SWITCH)) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
+                    }
+                }
             }
 
             // process the actions
@@ -217,6 +229,36 @@ public class Action {
                         (PowerManager) context.getSystemService(Context.POWER_SERVICE);
                 if (!powerManager.isScreenOn()) {
                     powerManager.wakeUp(SystemClock.uptimeMillis());
+                }
+                return;
+            } else if (action.equals(OwnActionConstants.ACTION_THEME_SWITCH)) {
+                boolean autoLightMode = Settings.Secure.getIntForUser(
+                        context.getContentResolver(),
+                        Settings.Secure.UI_THEME_AUTO_MODE, 0,
+                        UserHandle.USER_CURRENT) == 1;
+                boolean state = context.getResources().getConfiguration().uiThemeMode
+                        == Configuration.UI_THEME_MODE_HOLO_DARK;
+                if (autoLightMode) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
+                    }
+                    Toast.makeText(context,
+                            com.android.internal.R.string.theme_auto_switch_mode_error,
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Handle a switch change
+                // we currently switch between darktheme and lighttheme till either
+                // theme engine is ready or lighttheme is ready. Currently due of
+                // missing light themeing lighttheme = system base theme
+                final IUiModeManager uiModeManagerService = IUiModeManager.Stub.asInterface(
+                        ServiceManager.getService(Context.UI_MODE_SERVICE));
+                try {
+                    uiModeManagerService.setUiThemeMode(state
+                            ? Configuration.UI_THEME_MODE_HOLO_LIGHT
+                            : Configuration.UI_THEME_MODE_HOLO_DARK);
+                } catch (RemoteException e) {
                 }
                 return;
             } else {

core/java/com/android/internal/util/radium/ActionHelper.java
@@ -178,6 +178,9 @@ public class ActionHelper {
         } else if (clickAction.equals(RadiumActionConstants.ACTION_VIB_SILENT)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_ring_vib_silent", null, null);
+        } else if (clickAction.equals(OwnActionConstants.ACTION_THEME_SWITCH)) {
+            resId = systemUiResources.getIdentifier(
+                        SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_theme_switch", null, null);
         } else {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_null", null, null);