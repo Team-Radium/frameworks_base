ROM-Jeremy/android_frameworks_base

Add Slims DSLV & Missing Shortcut Icons
Change-Id: I7af9f809bdf90dc0b29b6a1cadc932f4a6f3aa30
Signed-off-by: OwnDroid <askowndroid@gmail.com>

core/java/com/android/internal/util/own/ActionHelper.java
@@ -107,13 +107,13 @@ public class ActionHelper {
             }
         }
 
-        if (customIcon != null && customIcon.startsWith(ActionConstants.SYSTEM_ICON_IDENTIFIER)) {
+        if (customIcon != null && customIcon.startsWith(OwnActionConstants.SYSTEM_ICON_IDENTIFIER)) {
             resId = systemUiResources.getIdentifier(customIcon.substring(
-                        ActionConstants.SYSTEM_ICON_IDENTIFIER.length()), "drawable", "android");
+                        OwnActionConstants.SYSTEM_ICON_IDENTIFIER.length()), "drawable", "android");
             if (resId > 0) {
                 return systemUiResources.getDrawable(resId);
             }
-        } else if (customIcon != null && !customIcon.equals(ActionConstants.ICON_EMPTY)) {
+        } else if (customIcon != null && !customIcon.equals(OwnActionConstants.ICON_EMPTY)) {
             File f = new File(Uri.parse(customIcon).getPath());
             if (f.exists()) {
                 return new BitmapDrawable(context.getResources(),
@@ -138,44 +138,44 @@ public class ActionHelper {
         int resId = -1;
 
         // ToDo: Add the resources to SystemUI.
-        if (clickAction.equals(ActionConstants.ACTION_HOME)) {
+        if (clickAction.equals(OwnActionConstants.ACTION_HOME)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_home", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_BACK)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_BACK)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_back", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_RECENTS)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_RECENTS)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_recent", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_SEARCH)
-                || clickAction.equals(ActionConstants.ACTION_ASSIST)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_SEARCH)
+                || clickAction.equals(OwnActionConstants.ACTION_ASSIST)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_search", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_KEYGUARD_SEARCH)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_KEYGUARD_SEARCH)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_search_light", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_MENU)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_MENU)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_menu", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_MENU_BIG)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_MENU_BIG)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_menu_big", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_IME)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_IME)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_ime_switcher", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_POWER)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_POWER)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_power", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_POWER_MENU)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_POWER_MENU)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_power_menu", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_VIB)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_VIB)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_vib", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_SILENT)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_SILENT)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_silent", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_VIB_SILENT)) {
+        } else if (clickAction.equals(OwnActionConstants.ACTION_VIB_SILENT)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_ring_vib_silent", null, null);
         } else {

core/java/com/android/internal/util/own/ConfigSplitHelper.java
@@ -43,7 +43,7 @@ public class ConfigSplitHelper {
         }
 
         // Split out the config to work with and add to the list
-        for (String configValue : config.split("\\" + ActionConstants.ACTION_DELIMITER)) {
+        for (String configValue : config.split("\\" + OwnActionConstants.ACTION_DELIMITER)) {
             counter++;
             if (counter == 1) {
                 actionConfig = new ActionConfig(configValue,
@@ -81,13 +81,13 @@ public class ConfigSplitHelper {
 
         for (int i = 0; i < actionConfigs.size(); i++) {
             if (i != 0) {
-                finalConfig += ActionConstants.ACTION_DELIMITER;
+                finalConfig += OwnActionConstants.ACTION_DELIMITER;
             }
             actionConfig = actionConfigs.get(i);
-            finalConfig += actionConfig.getClickAction() + ActionConstants.ACTION_DELIMITER;
+            finalConfig += actionConfig.getClickAction() + OwnActionConstants.ACTION_DELIMITER;
             if (!isShortcut) {
                 finalConfig += actionConfig.getLongpressAction()
-                    + ActionConstants.ACTION_DELIMITER;
+                    + OwnActionConstants.ACTION_DELIMITER;
             }
             finalConfig += actionConfig.getIcon();
         }

Team-Radium/frameworks_base

Conflicts:
	core/java/com/android/internal/util/own/Converter.java
	core/java/com/android/internal/util/radium/DeviceUtils.java

core/java/com/android/internal/util/radium/ActionHelper.java
@@ -107,13 +107,13 @@ public class ActionHelper {
             }
         }
 
-        if (customIcon != null && customIcon.startsWith(ActionConstants.SYSTEM_ICON_IDENTIFIER)) {
+        if (customIcon != null && customIcon.startsWith(RadiumActionConstants.SYSTEM_ICON_IDENTIFIER)) {
             resId = systemUiResources.getIdentifier(customIcon.substring(
-                        ActionConstants.SYSTEM_ICON_IDENTIFIER.length()), "drawable", "android");
+                        RadiumActionConstants.SYSTEM_ICON_IDENTIFIER.length()), "drawable", "android");
             if (resId > 0) {
                 return systemUiResources.getDrawable(resId);
             }
-        } else if (customIcon != null && !customIcon.equals(ActionConstants.ICON_EMPTY)) {
+        } else if (customIcon != null && !customIcon.equals(RadiumActionConstants.ICON_EMPTY)) {
             File f = new File(Uri.parse(customIcon).getPath());
             if (f.exists()) {
                 return new BitmapDrawable(context.getResources(),
@@ -138,44 +138,44 @@ public class ActionHelper {
         int resId = -1;
 
         // ToDo: Add the resources to SystemUI.
-        if (clickAction.equals(ActionConstants.ACTION_HOME)) {
+        if (clickAction.equals(RadiumActionConstants.ACTION_HOME)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_home", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_BACK)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_BACK)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_back", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_RECENTS)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_RECENTS)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_recent", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_SEARCH)
-                || clickAction.equals(ActionConstants.ACTION_ASSIST)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_SEARCH)
+                || clickAction.equals(RadiumActionConstants.ACTION_ASSIST)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_search", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_KEYGUARD_SEARCH)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_KEYGUARD_SEARCH)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_search_light", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_MENU)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_MENU)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_menu", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_MENU_BIG)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_MENU_BIG)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_menu_big", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_IME)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_IME)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_ime_switcher", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_POWER)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_POWER)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_power", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_POWER_MENU)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_POWER_MENU)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_power_menu", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_VIB)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_VIB)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_vib", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_SILENT)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_SILENT)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_silent", null, null);
-        } else if (clickAction.equals(ActionConstants.ACTION_VIB_SILENT)) {
+        } else if (clickAction.equals(RadiumActionConstants.ACTION_VIB_SILENT)) {
             resId = systemUiResources.getIdentifier(
                         SYSTEMUI_METADATA_NAME + ":drawable/ic_sysbar_ring_vib_silent", null, null);
         } else {

core/java/com/android/internal/util/radium/ConfigSplitHelper.java
@@ -43,7 +43,7 @@ public class ConfigSplitHelper {
         }
 
         // Split out the config to work with and add to the list
-        for (String configValue : config.split("\\" + ActionConstants.ACTION_DELIMITER)) {
+        for (String configValue : config.split("\\" + RadiumActionConstants.ACTION_DELIMITER)) {
             counter++;
             if (counter == 1) {
                 actionConfig = new ActionConfig(configValue,
@@ -81,13 +81,13 @@ public class ConfigSplitHelper {
 
         for (int i = 0; i < actionConfigs.size(); i++) {
             if (i != 0) {
-                finalConfig += ActionConstants.ACTION_DELIMITER;
+                finalConfig += RadiumActionConstants.ACTION_DELIMITER;
             }
             actionConfig = actionConfigs.get(i);
-            finalConfig += actionConfig.getClickAction() + ActionConstants.ACTION_DELIMITER;
+            finalConfig += actionConfig.getClickAction() + RadiumActionConstants.ACTION_DELIMITER;
             if (!isShortcut) {
                 finalConfig += actionConfig.getLongpressAction()
-                    + ActionConstants.ACTION_DELIMITER;
+                    + RadiumActionConstants.ACTION_DELIMITER;
             }
             finalConfig += actionConfig.getIcon();
         }