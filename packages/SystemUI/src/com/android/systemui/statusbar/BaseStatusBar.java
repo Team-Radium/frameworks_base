d10720ebb5dc45838c9f58bc84bf56d39913a689
Lars Greiss <kufikugel@googlemail.com>
2015-06-09 21:07:39 +0200

    SlimRecents: Update to PS19 (1/2)
    Change-Id: I42920ea9d2e789b07fd872e304ca24beded826c2
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1294,6 +1294,8 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected void hideRecents(boolean triggeredFromAltTab, boolean triggeredFromHomeKey) {
         if (mRecents != null) {
             mRecents.hideRecents(triggeredFromAltTab, triggeredFromHomeKey);
+        } else if (mSlimRecents != null) {
+                mSlimRecents.hideRecents(triggeredFromHomeKey);
         }
     }
 
@@ -1351,6 +1353,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                 Settings.System.USE_SLIM_RECENTS, 0, UserHandle.USER_CURRENT) == 1;
         if (slimRecents) {
             mSlimRecents = new RecentController(mContext, mLayoutDirection);
+            mSlimRecents.setCallback(this);
             mRecents = null;
         } else {
             mRecents = getComponent(RecentsComponent.class);
