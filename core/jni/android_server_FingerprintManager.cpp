7bc64df2fb7505c0ac3360d9cb4232226e42df2a
W4TCH0UT <ateekujjawal@gmail.com>
2015-07-01 16:21:36 +0530

    Revert "Frameworks/base: Fix FingerprintManager JNI"
    This reverts commit 21c3f079a18d19467936d05919dae9c38e3a06c2.
    
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>

--- core/jni/android_server_FingerprintManager.cpp
+++ core/jni/android_server_FingerprintManager.cpp
@@ -234,7 +234,7 @@ static jint nativeCloseHal(JNIEnv* env, jobject clazz) {
 static const JNINativeMethod g_methods[] = {
     { "nativeAuthenticate", "()I", (void*)nativeAuthenticate },
     { "nativeEnroll", "(I)I", (void*)nativeEnroll },
-    { "nativeEnrollCancel", "()I", (void*)nativeEnrollCancel },
+    { "nativeCancel", "()I", (void*)nativeCancel },
     { "nativeRemove", "(I)I", (void*)nativeRemove },
     { "nativeOpenHal", "()I", (void*)nativeOpenHal },
     { "nativeCloseHal", "()I", (void*)nativeCloseHal },
