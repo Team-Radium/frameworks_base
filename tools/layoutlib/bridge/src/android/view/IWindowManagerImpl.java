93193075ef2853f1faa1a208ccf0e50e52ac4a8c
cristianomatos <cristianobmatos@gmail.com>
2015-07-06 07:07:04 +0530

    HW Keys tweaks [2/2]
    Change-Id: I5879b883bd149c1566943bb71ed47ab3ddf3ab99
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    
    Conflicts:
    	core/res/res/values/cm_symbols.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    	tools/layoutlib/bridge/src/android/view/IWindowManagerImpl.java

--- tools/layoutlib/bridge/src/android/view/IWindowManagerImpl.java
+++ tools/layoutlib/bridge/src/android/view/IWindowManagerImpl.java
@@ -498,4 +498,15 @@ public class IWindowManagerImpl implements IWindowManager {
     @Override
     public void toggleGlobalMenu() {
     }
+
+    /* @hide */
+    @Override
+    public int getSystemUIVisibility() {
+        return 0;
+    }
+
+    @Override
+    public boolean hasPermanentMenuKey() {
+        return true;
+    }
 }
