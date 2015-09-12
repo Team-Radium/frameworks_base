c7a681d76ef62f2ba84ea101a88357282aab6c00
Alex Cruz <mazdarider23@gmail.com>
2015-09-12 00:24:25 -0600

    Fix weather colored icon size issue
    We could of fixed this another way by vectorizing the weather icons but this doesn't
    hurt anything and fixes the problem we had.
    
    After all, we have no one to impress....we just want a good ROM to enjoy :-)
    
    Credit/Thanks to SlimROMs for their ImageHelper, comes in handy!
    
    Change-Id: Iaa688aa11480f2220463c7654fc2ad8f1623131f
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3932,6 +3932,18 @@ public final class Settings {
         public static final String DISPLAY_AUTO_OUTDOOR_MODE = "display_auto_outdoor_mode";
 
         /**
+         * Color of the text on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_TEXT_COLOR = "lock_screen_weather_text_color";
+
+        /**
+         * Color of the icons on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_ICON_COLOR = "lock_screen_weather_icon_color";
+
+        /**
          * Change the color of the lockscreen bottom shortcuts
          *
          * @hide
